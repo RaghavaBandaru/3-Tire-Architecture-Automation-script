511a3806408f317ec69a301c6c600804ec41669c
Raghava <155598681+RaghavaBandaru@users.noreply.github.com>
2024-02-10 11:58:01 +1000

    Update tools_studentapp.sh

--- tools_studentapp.sh
+++ tools_studentapp.sh
@@ -47,10 +47,10 @@ sudo ln -s /opt/maven/bin/mvn /usr/bin/mvn
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.85/bin/apache-tomcat-9.0.85.tar.gz
 
 tar -xzvf apache-tomcat-9.0.85.tar.gz
-
+mv apache-tomcat-9.0.85 appserver
 rm -rf apache-tomcat-9.0.85.tar.gzr
 
-mv apache-tomcat-9.0.85 appserver
+
 
 # tomcat as a service(register)
 
