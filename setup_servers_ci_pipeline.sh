dc84fa223d2ef96e8fdd89805458de2e932d111c
Raghava <155598681+RaghavaBandaru@users.noreply.github.com>
2024-02-15 19:10:00 +1000

    Update setup_servers_ci_pipeline.sh

--- setup_servers_ci_pipeline.sh
+++ setup_servers_ci_pipeline.sh
@@ -27,8 +27,6 @@ sudo yum install tree wget zip unzip gzip vim net-tools git bind-utils python2-p
 sudo su - devops -c "git config --global user.name 'devops'"
 sudo su - devops -c "git config --global user.email 'devops@gmail.com'"
 
-# Installing Java 11
-# sudo yum install java-11-openjdk-devel.x86_64 -y
 
 sudo chown -R devops:devops /opt
 
@@ -36,11 +34,6 @@ sudo chown -R devops:devops /opt
 
 cd /opt
 
-wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
-tar -xzvf apache-maven-3.9.6-bin.tar.gz
-mv apache-maven-3.9.6 maven
-rm -rf apache-maven-3.9.6-bin.tar.gz
-sudo ln -s /opt/maven/bin/mvn /usr/bin/mvn
 
 # tomcat installation(deployment server)
 
@@ -99,9 +92,9 @@ systemctl status mariadb
 
 
 
-git clone https://gitlab.com/raghavabandaru854/student-app.git
-git clone https://gitlab.com/raghavabandaru854/static-project.git
-sudo yum install java-1.8.0-openjdk-devel.x86_64 -y
+# git clone https://gitlab.com/raghavabandaru854/student-app.git
+# git clone https://gitlab.com/raghavabandaru854/static-project.git
+# sudo yum install java-1.8.0-openjdk-devel.x86_64 -y
 
 
 
