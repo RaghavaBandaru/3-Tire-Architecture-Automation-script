c07191ce60fa96e63b89ed340dbda7386bbf8ead
Raghava <155598681+RaghavaBandaru@users.noreply.github.com>
2024-02-15 20:09:12 +1000

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -7,16 +7,16 @@ sudo setsebool -P httpd_can_network_connect 1
 mysql -uroot < ./dbscript/studentapp.sql
 
 # managers app context.xml
-sudo su devops - c "cp ./tomcat/manager/context.xml /opt/appserver/webapps/manager/META-INF"
+sudo su devops -c "cp ./tomcat/manager/context.xml /opt/appserver/webapps/manager/META-INF"
 
 # add user to the tomcat
-sudo su devops - c "cp ./tomcat/conf/tomcat-users.xml /opt/appserver/conf/"
+sudo su devops -c "cp ./tomcat/conf/tomcat-users.xml /opt/appserver/conf/"
 
 # load db driver
-sudo su devops - c "cp ./tomcat/lib/mysql-connector.jar /opt/appserver/lib/"
+sudo su devops -c "cp ./tomcat/lib/mysql-connector.jar /opt/appserver/lib/"
 
 # integrate tomcat with db
-sudo su devops - c "cp ./tomcat/conf/context.xml /opt/appserver/conf/"
+sudo su devops -c "cp ./tomcat/conf/context.xml /opt/appserver/conf/"
 
 # restart tomcat service
 
